8612700cfa12c2bca91657a7392a0abb831ee5d4
james-magee <jam020201@gmail.com>
2026-01-21 21:43:34 -0800

    removed build-manifest.mjs comment

--- scripts/build-manifest.mjs
+++ scripts/build-manifest.mjs
@@ -1,16 +1,5 @@
 import fs from "fs";
 
 const m = JSON.parse(fs.readFileSync("manifest.src.json", "utf8"));
-
-// TODO: dynamically add the stuff
-
-// // add everything from dist/*.js to manifest.json
-// try {
-//   const files = fs.readdirSync
-// }
-// catch (err) {
-//   console.log(err);
-// }
-
 m.content_scripts[0].js = ["content.js"];
 fs.writeFileSync("dist/manifest.json", JSON.stringify(m, null, 2));
